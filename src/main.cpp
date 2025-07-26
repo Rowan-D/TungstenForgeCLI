87238dc8574e45f727bad228c558c58173259f2a
Rowan-D <rowantigre@gmail.com>
2025-07-26 11:31:47 -0700

    Fixed File Path build errors

--- src/main.cpp
+++ src/main.cpp
@@ -14,11 +14,11 @@ int main(int argc, char** argv)
     const std::filesystem::path intDir = argv[3];
     const std::filesystem::path outputDir = argv[4];
 
-    W_DEBUG_LOG_INFO("Project Path: {}", inputPath.string());
-    W_DEBUG_LOG_INFO("Input Path: {}", inputPath.string());
-    W_DEBUG_LOG_INFO("Input Path: {}", inputPath.string());
-    W_DEBUG_LOG_INFO("Output Path: {}", outputPath.string());
+    W_DEBUG_LOG_INFO("Project Path: {}", projectPath.string());
+    W_DEBUG_LOG_INFO("Input Path: {}", wCoreSourceDir.string());
+    W_DEBUG_LOG_INFO("Input Path: {}", intDir.string());
+    W_DEBUG_LOG_INFO("Output Path: {}", outputDir.string());
 
     wForge::TungstenForge tungstenForge;
-    return !tungstenForge.BuildProject(projectPath, wCoreSourceDir, intDir, outputDir))
+    return !tungstenForge.BuildProject(projectPath, wCoreSourceDir, intDir, outputDir);
 }
