cd54e9747e7ec5da39d5af35bc0b7ea0edaa5e3d
Rowan-D <rowantigre@gmail.com>
2025-07-26 10:57:44 -0700

    Add Files

---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+#include "TungstenForge/TungstenForge.hpp"
+
+int main(int argc, char** argv)
+{
+    wUtils::TungstenLogger logger;
+    if (argc > 4)
+    {
+        logger.LogWarning("Expected 4 Arguments: projectPath, ");
+        return 1;
+    }
+
+    const std::filesystem::path projectPath = argv[1];
+    const std::filesystem::path wCoreSourceDir = argv[2];
+    const std::filesystem::path intDir = argv[3];
+    const std::filesystem::path outputDir = argv[4];
+
+    W_DEBUG_LOG_INFO("Project Path: {}", inputPath.string());
+    W_DEBUG_LOG_INFO("Input Path: {}", inputPath.string());
+    W_DEBUG_LOG_INFO("Input Path: {}", inputPath.string());
+    W_DEBUG_LOG_INFO("Output Path: {}", outputPath.string());
+
+    wForge::TungstenForge tungstenForge;
+    return !tungstenForge.BuildProject(projectPath, wCoreSourceDir, intDir, outputDir))
+}
